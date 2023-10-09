2aacd12c46421df121e7d96cb4b4d21aed4508fb
DiegoP2001 <di.piedra@aulanz.net>
2023-10-09 12:33:16 +0200

    Logica Implementada

---
+++ js/playerName.js
@@ -0,0 +1,23 @@
+//ruta 2da pagina './game.html'
+
+function clickEmpezar(){
+    const link = document.querySelector(".link");
+    const input = document.querySelector(".input.is-primary");
+
+    if (input.value === '') {
+        alert("Introduzca un nombre");
+    } else {
+    
+    let name = input.value; 
+
+    localStorage.setItem("Nombre", name);
+    link.setAttribute('href', './game.html');
+
+    console.log(link)
+    console.log("Hola")
+}
+}
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let matches = 0;
+let score = 0
+let progressBar = document.getElementById("miProgress");
+const nombre = localStorage.getItem("Nombre");
+const nombreTitulo = document.querySelector('.title.is-4')
+nombreTitulo.textContent = nombre;
+
 function generar_pos_aleatoria_cartas(){
   
   const src = {
@@ -37,7 +44,7 @@ function generar_numero_aleatorio(indexArray){
   return indexArray;
 }
 
-let giradas = []
+let giradas = [];
 
 
 function girar_carta() {
@@ -45,24 +52,22 @@ function girar_carta() {
   
   cartas.forEach(element => {
     element.addEventListener("click", function() {
-      this.style.transform = "rotateY(180deg)";
-      this.classList.add("girada")
+        this.style.transform = "rotateY(180deg)";
+        this.classList.add("girada")
       });
   });
 
 
 }
 
-
-  
 girar_carta();
 
-// Función para verificar si dos cartas son iguales (personalizar según tus necesidades)
-function sonIguales(carta1, carta2) {
+
+/*function sonIguales(carta1, carta2) {
   const img1 = carta1.querySelector(".carta > .cara.detras > img");
   const img2 = carta2.querySelector(".carta > .cara.detras > img");
   return img1.getAttribute("src") === img2.getAttribute("src");
-}
+}*/
 
 
 function comprobar_igualdad_cartas(){
@@ -71,9 +76,11 @@ function comprobar_igualdad_cartas(){
     let cartas_boca_abajo = null;
     let carta1 = null;
     let carta2 = null
-
+    
+    
     setTimeout(() => {
        cartas_giradas = document.querySelectorAll(".carta.girada")
+       cartas_boca_abajo = document.querySelectorAll(".carta")
        console.log(cartas_boca_abajo)
        console.log(cartas_giradas)
        console.log(carta1 + " Carta1")
@@ -82,8 +89,7 @@ function comprobar_igualdad_cartas(){
     
     setTimeout(() => {
       if (cartas_giradas.length == 2){
-
-
+        
         carta1 = cartas_giradas[0].querySelector(".cara.detras > img").getAttribute("src");
         carta2 =  cartas_giradas[1].querySelector(".cara.detras > img").getAttribute("src");
         console.log(carta1 + " Carta1 dentro de array = 2")
@@ -96,15 +102,19 @@ function comprobar_igualdad_cartas(){
           cartas_giradas[1].style.transform = "rotateY(0deg)"
           
           
+          
         }else{
           cartas_giradas[0].classList.remove("girada")
           cartas_giradas[1].classList.remove("girada")
-          //cartas_giradas[0].style.display = "none"
-          //cartas_giradas[1].style.display = "none"
           cartas_giradas[0].style.backgroundColor = "black"
-          cartas_giradas[1].style.backgroundColor = "black"
+          cartas_giradas[1].style.backgroundColor = "black"  
+          cartas_giradas[0].style.pointerEvents = "none"
+          cartas_giradas[1].style.pointerEvents = "none"
+          matches++;
+          let intervalo = 60 * 1000 / progressBar.max;;
+          score += (progressBar.value * intervalo)
           
-          
+
           cartas_giradas = null
         }
           cartas_giradas = null;
@@ -112,19 +122,82 @@ function comprobar_igualdad_cartas(){
 
     }, 1000)
 
-    girar_carta()
+    //girar_carta()
     
 }
 
-
-
-
-function cartas_iguales(){
+document.addEventListener("DOMContentLoaded", function () {
   
+  let duracion = 60; 
+  let intervalo = duracion * 1000 / progressBar.max;; 
+  
+  function actualizarProgressBar() {
+    if (progressBar.value > 0) {
+      progressBar.value -= 1;
+      hasGanado(progressBar.value, nombre, score, intervalo)
+      if(matches === 10){
+        clearInterval(intervalID)
+      }
+    } else {
+      clearInterval(intervalID);
+      hasPerdido()
+    }
+  }
+
+  let intervalID = setInterval(actualizarProgressBar, intervalo);
+});
+
+
+function hasGanado(progressBarValue, nombre, score, intervalo){
+  
+  if((progressBarValue > 0 && matches === 10)){
+    limpiarPantalla()
+    
+    
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('info-container');
+
+
+    const infoBox = document.createElement('div');
+    infoBox.classList.add('info-box');
+
+
+    const nombreParagraph = document.createElement('p');
+    nombreParagraph.innerHTML = `Nombre: <span id="nombre">${nombre}</span>`;
+
+    const scoreParagraph = document.createElement('p');
+    scoreParagraph.innerHTML = `Puntaje: <span id="score">${score}</span>`;
+
+    const tiempoParagraph = document.createElement('p');
+    tiempoParagraph.innerHTML = `Tiempo: <span id="tiempo">${60 - ((progressBarValue * intervalo) / 1000)}seg</span>`;
+
+
+    infoBox.appendChild(nombreParagraph);
+    infoBox.appendChild(scoreParagraph);
+    infoBox.appendChild(tiempoParagraph);
+
+
+    infoContainer.appendChild(infoBox);
+
+
+    document.body.appendChild(infoContainer);
+
+  }
+}
+
+function hasPerdido(){
+
+}
+
+function limpiarPantalla(){
+  const body = document.body;
+
+  while (body.firstChild) {
+    body.removeChild(body.firstChild);
+  }
 }
 
 
 
-function comenzar_temporizador(){
 
-}
+
